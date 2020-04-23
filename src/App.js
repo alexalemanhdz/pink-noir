b02139a7af9e1712828c694c4cd0a790dd61ea5a
alexalemanhdz <alex.aleman.public@gmail.com>
2020-04-23 14:35:25 -0500

    Added links arrays

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import workLinks from './work-links';
+import funLinks from './fun-links';
+
+const displayMapping = (linksArray) => linksArray.map((link) => (
+  <li key={`work-link-${link.id}`}>
+    <a href={link.url}>{link.displayName}</a>
+  </li>
+));
 
 function App() {
+  const workDisplayLinks = displayMapping(workLinks);
+  const funDisplayLinks = displayMapping(funLinks);
+
   return (
     <div className="App">
       <header className="App-header">
-        <div className="Tab-container" />
+        <div className="Tab-container">
+          <div className="Work-tab">
+            <ul>{workDisplayLinks}</ul>
+          </div>
+          <div className="Fun-tab">
+            <ul>{funDisplayLinks}</ul>
+          </div>
+        </div>
         <div className="Media-container" />
       </header>
     </div>
---
+++ src/links.example.js
@@ -0,0 +1,9 @@
+// Create both files work-links.js and fun-links.js, and use this format
+
+export default [
+  {
+    id: 'google',
+    displayName: 'google',
+    url: 'https://google.com',
+  },
+];
