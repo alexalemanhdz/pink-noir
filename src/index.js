5f3922f4ae9c1134e8bfe7025616580dda9e589a
alexalemanhdz <alex.aleman.public@gmail.com>
2020-04-24 13:13:08 -0500

    Reset redux store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
-import store from './store';
-
 import './index.css';
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root'),
 );
 
