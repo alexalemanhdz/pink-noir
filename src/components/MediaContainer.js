65f2fbe7a5193165baf65dce31857960f6f8ccf4
alexalemanhdz <alex.aleman.public@gmail.com>
2020-04-25 00:16:13 -0500

    koreaboo

--- src/components/MediaContainer.js
+++ src/components/MediaContainer.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import '../styles/MediaContainer.css';
 import woosh from '../assets/woosh.jpg';
+import jennie from '../assets/jennie.gif';
 
 function MediaContainer(props) {
   let media = null;
+  const { currentTab } = props;
 
-  if (props.currentTab === 0) {
-    media = <div className="woosh" style={{ backgroundImage: `url(${woosh})` }} />;
+  switch (currentTab) {
+    case 0:
+      media = <div className="woosh" style={{ backgroundImage: `url(${woosh})` }} />;
+      break;
+    case 1:
+      media = <img className="jennie" src={jennie} alt="Jennie" />;
+      break;
+    default:
+      media = null;
   }
 
   return (
