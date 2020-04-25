644b5adf1c075caa5894a7d5a8c41953d8315652
alexalemanhdz <alex.aleman.public@gmail.com>
2020-04-25 00:09:47 -0500

    woosh

--- src/components/MediaContainer.js
+++ src/components/MediaContainer.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import '../styles/MediaContainer.css';
+import woosh from '../assets/woosh.jpg';
 
 function MediaContainer(props) {
+  let media = null;
+
+  if (props.currentTab === 0) {
+    media = <div className="woosh" style={{ backgroundImage: `url(${woosh})` }} />;
+  }
+
   return (
     <div className="media-container">
-      {props.currentTab}
+      {media}
     </div>
   );
 }
